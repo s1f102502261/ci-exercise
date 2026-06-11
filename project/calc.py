ea022a7d9f5e08abba8dd82ec8ce5dc5b46ca909
1F10250226 <s1f102502261@iniad.org>
2026-06-11 14:53:13 +0900

    因数が負の場合、例外を発生

--- project/calc.py
+++ project/calc.py
@@ -1,7 +1,7 @@
 def fact(n):
     if n < 0:
         raise ValueError("n must be a non-negative integer")
-            result = 1
+    result = 1
     for i in range(1, n + 1):
         result *= i
     return result
