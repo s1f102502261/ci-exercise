79050de9f1ee07e5401a01b09d8216c2e822c684
1F10250226 <s1f102502261@iniad.org>
2026-06-11 15:36:40 +0900

    最大公約数を返す関数を追加

--- project/calc.py
+++ project/calc.py
@@ -5,3 +5,23 @@ def fact(n):
     for i in range(1, n + 1):
         result *= i
     return result
+
+def gcd(a, b):
+    a = abs(a)
+    b = abs(b)
+    while b != 0:
+        a, b = b, a % b
+        
+    return a
+    def test_gcd_positive(self):
+        self.assertEqual(gcd(7, 3), 1)
+        self.assertEqual(gcd(56, 98), 14)
+
+    def test_gcd_negative(self):
+        self.assertEqual(gcd(-15, -25), 5)
+        self.assertEqual(gcd(-24, 32), 8)
+
+    def test_gcd_zero(self):
+        self.assertEqual(gcd(5, 0), 5)
+        self.assertEqual(gcd(0, 7), 7)
+        self.assertEqual(gcd(0, 0), 0)
