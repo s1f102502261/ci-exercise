5209b23a6490f556d8ac423d270fc8ef307dc301
1F10250226 <s1f102502261@iniad.org>
2026-06-11 15:43:36 +0900

    testに追加

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -11,3 +11,16 @@ class TestTarget(unittest.TestCase):
     def test_fact_negative(self):
         with self.assertRaises(ValueError):
             fact(-1)
+
+    def test_gcd_positive(self):
+        self.assertEqual(gcd(7, 3), 1)
+        self.assertEqual(gcd(56, 98), 14)
+
+    def test_gcd_negative(self):
+        self.assertEqual(gcd(-15, -25), 5)
+        self.assertEqual(gcd(-24, 32), 8)
+
+    def test_gcd_zero(self):
+        self.assertEqual(gcd(5, 0), 5)
+        self.assertEqual(gcd(0, 7), 7)
+        self.assertEqual(gcd(0, 0), 0)
