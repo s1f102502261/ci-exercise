d00d38c320cfee5a6d63480dd1f316f72f927d3c
1F10250226 <s1f102502261@iniad.org>
2026-06-11 15:39:22 +0900

    最大公約数を返す関数を追加

--- project/calc.py
+++ project/calc.py
@@ -13,15 +13,3 @@ def gcd(a, b):
         a, b = b, a % b
         
     return a
-    def test_gcd_positive(self):
-        self.assertEqual(gcd(7, 3), 1)
-        self.assertEqual(gcd(56, 98), 14)
-
-    def test_gcd_negative(self):
-        self.assertEqual(gcd(-15, -25), 5)
-        self.assertEqual(gcd(-24, 32), 8)
-
-    def test_gcd_zero(self):
-        self.assertEqual(gcd(5, 0), 5)
-        self.assertEqual(gcd(0, 7), 7)
-        self.assertEqual(gcd(0, 0), 0)
