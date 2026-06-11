d2a36415ffaada81b31c0b730201f730464fe89d
1F10250226 <s1f102502261@iniad.org>
2026-06-11 15:12:07 +0900

    テストメソッドを追加

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -4,3 +4,10 @@ class TestTarget(unittest.TestCase):
     def test_fact_positive(self):
         self.assertEqual(fact(1), 1)
         self.assertEqual(fact(5), 120)
+
+    def test_fact_zero(self):
+        self.assertEqual(fact(0), 1)
+
+    def test_fact_negative(self):
+        with self.assertRaises(ValueError):
+            fact(-1)
